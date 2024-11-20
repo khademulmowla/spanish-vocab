daeb4220d7a2ebcf41272453c7418b0b21b1b035
khademul mowla <khademulmowla31eee@gmail.com>
2024-11-21 00:48:42 +0600

    installed react-countup

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCrgm6xbf-P09g3TyP9R1u96HmYXAhtyXg",
-    authDomain: "assignment-9-14df0.firebaseapp.com",
-    projectId: "assignment-9-14df0",
-    storageBucket: "assignment-9-14df0.firebasestorage.app",
-    messagingSenderId: "643870603003",
-    appId: "1:643870603003:web:a7af7df0238007e907625e"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
