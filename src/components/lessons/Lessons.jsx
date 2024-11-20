38ff3bd05a865aeac4fece66a49b5cb50f586be1
khademul mowla <khademulmowla31eee@gmail.com>
2024-11-21 02:23:44 +0600

    added startlearning and lessons

--- src/components/lessons/Lessons.jsx
+++ src/components/lessons/Lessons.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
+import Modal from '../modal/Modal';
+import { useState } from 'react';
 
 const Lessons = () => {
-    return (
-        <div>
+    const vocabData = useLoaderData();
+    const { lesson_no } = useParams();
+    const navigate = useNavigate();
+    const [modalData, setModalData] = useState(null);
+    const lessonVocabData = vocabData.filter(item => item.Lesson_no.toString() === lesson_no);
+    function pronounceWord(word) {
+        const utterance = new SpeechSynthesisUtterance(word);
+        utterance.lang = 'ja-JP';
+        window.speechSynthesis.speak(utterance);
+    }
+    const handleOpenModal = (data) => {
+        setModalData(data);
+        document.getElementById('my_modal_5').showModal();
+    };
 
+    return (
+        <div className="container mx-auto px-4 py-4">
+            <h1 className="text-3xl text-blue-900 font-bold mb-6 text-center">Lesson {lesson_no}</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {lessonVocabData.map((item) => (
+                    <div onClick={() => pronounceWord(item.word)} key={item.Id} className={`vocab-card p-4 rounded-lg shadow-md ${item.difficulty === 'easy'
+                        ? 'bg-green-100'
+                        : item.difficulty === 'medium'
+                            ? 'bg-yellow-100'
+                            : 'bg-red-100'
+                        }`}>
+                        <p className="font-semibold text-xl">{item.word}</p>
+                        <p><strong>Meaning:</strong> {item.meaning}</p>
+                        <p><strong>Pronunciation:</strong> {item.pronunciation}</p>
+                        <p><strong>Part of Speech:</strong> {item.part_of_speech}</p>
+                        <button
+                            className="mt-4 px-4 py-2 bg-sky-700 text-white rounded-lg hover:bg-teal-600"
+                            onClick={() => handleOpenModal({
+                                word: item.word,
+                                meaning: item.meaning,
+                                when_to_say: item.when_to_say,
+                                example: item.example
+                            })}
+                        >
+                            When to say
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <Modal modalData={modalData}> </Modal>
+            <button
+                className="mt-8 px-6 py-3 bg-blue-900 text-white rounded-lg hover:bg-teal-600"
+                onClick={() => navigate('/startlearning')}
+            >
+                Back to Lesson
+            </button>
         </div>
     );
 };
--- src/components/startLearning/StartLearning.jsx
+++ src/components/startLearning/StartLearning.jsx
@@ -1,8 +1,53 @@
+import React from 'react';
+import { useLoaderData, NavLink, Link } from 'react-router-dom';
+import pinImg from '../../assets/pin.png'
 
 const StartLearning = () => {
-    return (
-        <div>
+    const vocabData = useLoaderData();
 
+    return (
+        <div className='w-11/12 mx-auto'>
+            <h1 className="text-3xl font-bold text-center text-blue-900 my-8">Let's Start Learning!</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+                {vocabData.slice(0, 10).map((item) => (
+                    <NavLink
+                        key={item.Id}
+                        to={`/lessons/${item.Lesson_no}`}
+                        className="bg-teal-600 text-white text-lg font-semibold py-4 px-6 rounded-lg shadow-md hover:bg-orange-600 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1"
+                    >
+                        <h3 className="text-center">Lesson {item.Lesson_no}</h3>
+                    </NavLink>
+                ))}
+            </div>
+            <div className="tutorial-section my-10">
+                <div className='text-center mb-20'>
+                    <h2 className="text-3xl text-center font-bold text-blue-900 mb-4 transform -rotate-6 inline-block px-4 py-2 rounded-lg shadow-md">
+                        Learn the Alphabet
+                    </h2>
+                </div>
+                <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between mt-8 gap-6">
+                    <div className="flex justify-center w-full lg:w-1/2">
+                        <iframe className='rounded-lg shadow-lg border border-gray-500 p-2 ' width="560" height="315" src="https://www.youtube.com/embed/dlazjKbt40Q?si=EKUVC5_1wDI-_ORL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                    </div>
+                    <div className="flex flex-col space-y-3 justify-center items-center text-center w-full lg:w-1/2">
+                        <img
+                            src={pinImg}
+                            alt=""
+                            className="w-32 h-20 object-contain"
+                        />
+                        <h3 className="text-2xl font-semibold text-gray-800">
+                            Let our native speakers guide you through language that’s real, relevant, and authentic
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center mb-14 pt-5">
+                <Link to="/tutorials"
+                    className="bg-blue-900 text-white text-lg font-semibold py-3 px-6 rounded-full hover:bg-teal-600  shadow-md">
+                    View More
+                </Link>
+            </div>
         </div>
     );
 };
