05d592e02d1bde659f349a7f8acf79bdee97871d
khademul mowla <khademulmowla31eee@gmail.com>
2024-11-21 17:58:29 +0600

    updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Banner from '../banner/Banner';
+import Success from '../success/Success';
+import AboutSection from '../aboutSection/AboutSection';
+import { Outlet } from 'react-router-dom';
+import VocabSchool from '../vocabSchool/VocabSchool';
 
 const Home = () => {
     return (
         <div>
-
+            <div className='w-11/12 mx-auto'>
+                <Banner></Banner>
+                <AboutSection></AboutSection>
+                <Success></Success>
+                <Outlet></Outlet>
+                <VocabSchool></VocabSchool>
+            </div>
         </div>
     );
 };
